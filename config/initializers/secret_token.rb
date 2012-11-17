840937c4421a7df8bc4a525b23b53de4394e7804
David <ddk128@gmail.com>
2012-11-18 00:22:26 +0100

    Add initial project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Financials::Application.config.secret_token = 'b3662244b79694f120c9ad5b4ed3d1735a8a65f99501ce44a8cb6c575e750d4d09749765e41ba429606d0e64270b7d5aecde4ca0616fa321f8f8858eada94512'
